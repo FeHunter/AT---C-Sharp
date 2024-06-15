5f67e6a9b92fc0efc7fe5ceab95c468b3aee9588
FeHunter <hunterfe1111@gmail.com>
2024-06-15 12:06:23 -0300

    Iniciando Menu no Main

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace AtChsarp
     {
         static void Main ()
         {
-            
+            ContaCorrente conta1 = new ContaCorrente(0, 300);
+
+            bool fim = false;
+
+            while (!fim){
+                MostrarMenu();
+            }
+        }
+
+        static public void MostrarMenu ()
+        {
+            Console.WriteLine("1 - Sacar");
+            Console.WriteLine("2 - Apresentar o saldo");
+            Console.WriteLine("3 - Depositar");
+            Console.WriteLine("4 - Sair");
         }
     }
 }
