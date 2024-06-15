0dcdfffc2f993aca1ab21c6250033156c7f21116
FeHunter <hunterfe1111@gmail.com>
2024-06-15 12:35:24 -0300

    Ajuste finais

--- obj/Debug/net8.0/AT.AssemblyInfo.cs
+++ obj/Debug/net8.0/AT.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21aaea5d192dcfe75a3aba36390f6bae975139da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c54aa7f2c0aff486ebb37aa3857d9af0f2470f33")]
 [assembly: System.Reflection.AssemblyProductAttribute("AT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
