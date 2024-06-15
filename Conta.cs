35a1bf0cb0cc2620940b82440ebb4c0236c00d1f
FeHunter <hunterfe1111@gmail.com>
2024-06-15 11:17:29 -0300

    Correção deixar saldo somente como leitura, e impedir limite negativo.

--- Conta.cs
+++ Conta.cs
@@ -3,11 +3,11 @@ abstract class Conta
     private double _saldo;
     private double _limite;
 
-    public double Saldo { get => _saldo; set {
+    public double Saldo { get => _saldo; }
+    public double Limite { get => _limite; set {
         if (value > 0)
         {
-            _saldo = value;
+            _limite = value;
         }
-    }}
-    public double Limite { get => _limite; set => _limite = value; }
+    } }
 }
