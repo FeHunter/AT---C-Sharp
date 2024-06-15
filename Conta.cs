5ca32d48946cc3ef8bdac7512cda8cfd12db0fed
FeHunter <hunterfe1111@gmail.com>
2024-06-15 11:25:05 -0300

    Correção em Conta.

--- Conta.cs
+++ Conta.cs
@@ -3,26 +3,15 @@ abstract class Conta
     private double _saldo;
     private double _limite;
 
-    public double Saldo { get => _saldo; }
-    public double Limite { get => _limite; set {
+    public double Saldo { get => _saldo; private set => _saldo = value; }
+    public double Limite { get => _limite; private set {
         if (value > 0)
         {
             _limite = value;
         }
     } }
 
-    public virtual void Depositar (double valor)
-    {
-        if (valor > 0){
-            _saldo += valor;
-        }
-    }
+    public abstract void Depositar (double valor);
 
-    public virtual void Sacar (double valor)
-    {
-        if (valor <= _saldo)
-        {
-            _saldo -= valor;
-        }
-    }
+    public abstract void Sacar (double valor);
 }
