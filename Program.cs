5116d71cba5d793dac73eb26fb96f5678f5cda62
FeHunter <hunterfe1111@gmail.com>
2024-06-15 12:08:14 -0300

    Ajuster no menu

--- Program.cs
+++ Program.cs
@@ -11,16 +11,18 @@ namespace AtChsarp
             bool fim = false;
 
             while (!fim){
-                MostrarMenu();
+                int op = Menu();
             }
         }
 
-        static public void MostrarMenu ()
+        static public int Menu ()
         {
             Console.WriteLine("1 - Sacar");
             Console.WriteLine("2 - Apresentar o saldo");
             Console.WriteLine("3 - Depositar");
             Console.WriteLine("4 - Sair");
+            int op = int.Parse(Console.ReadLine());
+            return op;
         }
     }
 }
