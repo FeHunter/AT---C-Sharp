14059f3be917702bf7d29a365ba00c94c7369c63
FeHunter <hunterfe1111@gmail.com>
2024-06-15 11:38:11 -0300

    Criada classe ContaCorrente e correção em Conta.

--- Conta.cs
+++ Conta.cs
@@ -1,17 +1,26 @@
-abstract class Conta
+namespace AtChsarp
 {
-    private double _saldo;
-    private double _limite;
+    abstract class Conta
+    {
+        private double _saldo;
+        private double _limite;
 
-    public double Saldo { get => _saldo; private set => _saldo = value; }
-    public double Limite { get => _limite; private set {
-        if (value > 0)
+        public double Saldo { get => _saldo; protected set => _saldo = value; }
+        public double Limite { get => _limite; private set {
+            if (value > 0)
+            {
+                _limite = value;
+            }
+        } }
+
+        public Conta (double saldo, double limite)
         {
-            _limite = value;
+            Saldo = saldo;
+            Limite = limite;
         }
-    } }
 
-    public abstract void Depositar (double valor);
+        public abstract void Depositar (double valor);
 
-    public abstract void Sacar (double valor);
+        public abstract void Sacar (double valor);
+    }
 }
---
+++ ContaCorrente.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace AtChsarp
+{
+    class ContaCorrente : Conta
+    {
+        public ContaCorrente(double saldo, double limite) : base(saldo, limite)
+        {
+        }
+
+        public override void Depositar(double valor)
+        {
+            if (valor > 0)
+            {
+                Saldo += valor;
+            }
+        }
+
+        public override void Sacar(double valor)
+        {
+            if (valor <= Saldo)
+            {
+                Saldo -= valor;
+            }
+        }
+    }
+}
